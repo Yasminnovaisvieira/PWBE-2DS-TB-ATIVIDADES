48c38e6ed6b0857f357208fd895e252e023c3164
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-08-07 16:17:41 -0300

    PWBE - 07/08/2025

--- api/models.py
+++ api/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
 class Autor(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255)
-    data_nascimento = models.DateField(null = True, blank = True) # null = True: Pode deixar vazio
-    nacion = models.CharField(max_length = 30, null = True, blank = True)
-    biografia = models.TextField(null = True, blank = True) 
+    data_nascimento = models.DateField(null=True, blank=True)
+    nacion = models.CharField(max_length=30, null=True, blank=True)
+    biogra = models.TextField(null=True, blank=True)
+
