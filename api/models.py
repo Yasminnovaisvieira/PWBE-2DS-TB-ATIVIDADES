b996652c6be22a385c1f0ebd0335ef636e1f1411
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-08-07 14:53:05 -0300

    PWBE - 07/08/2025

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    data_nascimento = models.DateField(null = True, blank = True) # null = True: Pode deixar vazio
+    nacion = models.CharField(max_length = 30, null = True, blank = True)
+    biografia = models.TextField(null = True, blank = True) 
