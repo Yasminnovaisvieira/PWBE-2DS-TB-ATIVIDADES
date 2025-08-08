7c326c480a7a10b0be5b1660c55643b19c8e7564
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-08-08 16:31:18 -0300

    PWBE - 08/08/2025

--- api/models.py
+++ api/models.py
@@ -4,8 +4,8 @@ class Autor(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255)
     data_nascimento = models.DateField(null=True, blank=True)
-    nacion = models.CharField(max_length=30, null=True, blank=True)
-    biogra = models.TextField(null=True, blank=True)
+    nacao = models.CharField(max_length=30, null=True, blank=True)
+    biografia = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.nome} {self.sobrenome}"
