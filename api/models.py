9c4fc5d3fa42ba343d8d4cd174f6152363f27f27
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-08-21 13:55:37 -0300

    PWBE - 21/08/2025

--- api/models.py
+++ api/models.py
@@ -15,3 +15,31 @@ class Autor(models.Model):
     # Usado no admin, shell e logs
     def __str__(self):
         return f"{self.nome} {self.sobrenome}" # Retorna o nome completo do autor
+    
+class Editora(models.Model):
+    editora = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=18, unique=True, null=True, blank=True)
+    endereco = models.CharField(max_length=200, null=True, blank=True)
+    telefone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True) # EmailField = Regras de E-mail
+    site = models.URLField(null=True, blank=True) # URLField = 
+
+# blank = True : Não é preciso preencher o campo.
+# null = True : Aí se eu deixar vazio, o valor será NULL
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    subtitulo = models.CharField(max_length=255)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE) #Pega da tabela Autor
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE) #Pega da tabela editora
+    isbn = models.CharField(max_length=255)
+    descricao = models.TextField()
+    idioma = models.CharField(max_length=255, default="Português")
+    ano_publicado = models.IntegerField()
+    paginas = models.IntegerField()
+    preco = models.DecimalField(max_digits=10,decimal_places=2)
+    estoque = models.IntegerField()
+    desconto = models.DecimalField(max_digits=10, decimal_places=2)
+    disponivel =models.BooleanField(default=True)
+    dimensoes = models.CharField(max_length=255)
+    peso = models.DecimalField(max_digits=10, decimal_places=2)
