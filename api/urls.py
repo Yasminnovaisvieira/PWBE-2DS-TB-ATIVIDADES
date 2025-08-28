2643ad49baf9ccfce49cf04655afed0de1e693b5
Yasminnovaisvieira <Yasminnovaisvieira789@gmail.com>
2025-08-28 16:24:43 -0300

    PWBE - 28/08/2025

--- api/urls.py
+++ api/urls.py
@@ -7,25 +7,22 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import permissions
 
 # Lista as rotas de aplicação
 urlpatterns = [
     # GET / POST
-    path('autores', AutoresView.as_view()), #Cria a rota '/autores' e quando o usuário acessar, a view 'AutoresView' será chamada
-    # '.as_view()' = Transforma a classe em uma função que o Django consegue usar
-    path('authors', visualizacao_autor),
-    path('editoras', EditorasView.as_view()),
-    path('livros', LivrosView.as_view()),
+    path('autores/', AutoresView.as_view()),
+    path('authors/', visualizacao_autor),
+    path('editoras/', EditorasView.as_view()),
+    path('livros/', LivrosView.as_view()),
 
-    # UPDATE / DELETE (NÃO DEU CERTO)
-    path('autor/<int:pk>', AutoresDetailView.as_view()),
-    path('editora/<int:pk>', EditorasDetailView.as_view()),
-    path('livro/<int:pk>', LivrosDetailView.as_view())
+    # UPDATE / DELETE
+    path('autor/<int:pk>/', AutoresDetailView.as_view()),
+    path('editora/<int:pk>/', EditorasDetailView.as_view()),
+    path('livro/<int:pk>/', LivrosDetailView.as_view()),
+
+    # JWT
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-# IMPORTANTE: Normalmente se adiciona uma barra no final ('autores/') para seguir o padrão REST do Django
-# REST: Estilo de arquitetura para criar APIs que seguem algumas regras e padrões para comunicação entre cliente e servidor
-
-path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
